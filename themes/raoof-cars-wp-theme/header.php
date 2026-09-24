b8edb60e40751ada9d4f6f2c44e66fc092969091
Nehad <nehad.al.timimi@gmail.com>
2026-09-24 18:26:21 +0200

    test  ahmed dev

--- themes/raoof-cars-wp-theme/header.php
+++ themes/raoof-cars-wp-theme/header.php
@@ -16,6 +16,6 @@ if (!defined('ABSPATH')) {
 	<?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>>
-	<h1>Test Ci/Cd</h1>
+	<h1>Hallo Ahmed code</h1>
 <?php wp_body_open(); ?>
 <?php get_template_part('template-parts/navbar'); ?>
